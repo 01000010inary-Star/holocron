6d53e05a6f226c3c64c9f632e8c3dddb05709d7b
Joshua Seligman <joshuaseligman18@gmail.com>
2024-10-05 09:32:17 -0400

    Starter configs to get started

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 80,
+};
+
+export default config;
