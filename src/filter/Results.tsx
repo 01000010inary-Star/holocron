da86bc0a84f3ff736e8bbc812e510ecbf48b2688
Alexander Stanga <alexstanga99@gmail.com>
2024-10-05 15:11:21 -0600

    Initial commit of Results drawer

---
+++ src/filter/Results.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
+
+const Results: React.FC = () => {
+
+}
