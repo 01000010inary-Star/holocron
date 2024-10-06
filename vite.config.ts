c5fce458599cdfa3a0d8f7012158921e338f29ae
Joshua Seligman <joshuaseligman18@gmail.com>
2024-10-06 15:10:10 -0400

    Adding back in the base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/holocron",
     plugins: [
         react(),
         wasm(),
